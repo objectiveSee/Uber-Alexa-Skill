02d34bd21dec55f1f8d3666b5476d908d3362060
objectiveSee <dan.ricciotti@gmail.com>
2015-10-03 23:35:57 -0400

    Initial commit!

---
+++ README.md
@@ -0,0 +1,6 @@
+== UBER ALEXA PROJECT ==
+
+`grunt deploy`
+
+See `grundFile.js`. 
+See https://github.com/Tim-B/grunt-aws-lambda
---
+++ gruntFile.js
@@ -0,0 +1,37 @@
+var grunt = require('grunt');
+grunt.loadNpmTasks('grunt-aws-lambda');
+
+grunt.initConfig({
+   lambda_invoke: {
+      default: {
+         // options: {
+         //    file_name: 'CreateThumbnail.js'
+         // }
+      }
+   },
+   lambda_deploy: {
+   	  
+      default: {
+         // function: 'CreateThumbnail'
+         arn : 'arn:aws:lambda:us-east-1:017523382944:function:UberAlexaLambdaFunction',
+         options : {
+         	region : 'us-east-1',
+         	profile : 'personal',
+         	file_name: 'uber-alexa.js'
+      	}
+
+      },
+   },
+   lambda_package: {
+      default: {
+      	arn : 'arn:aws:lambda:us-east-1:017523382944:function:UberAlexaLambdaFunction',
+         options : {
+         	region : 'us-east-1',
+         	profile : 'personal',
+         	file_name: 'uber-alexa.js'
+      	}
+   	  }
+   }
+});
+
+grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy'])
